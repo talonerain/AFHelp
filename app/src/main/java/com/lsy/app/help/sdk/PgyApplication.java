0b78f010762409aeff04e267854649cc2e039757
liusiyu.taloner <1363431425@qq.com>
2018-03-22 18:14:06 +0800

    tinker gradle配置

--- app/src/main/java/com/lsy/app/help/sdk/PgyApplication.java
+++ app/src/main/java/com/lsy/app/help/sdk/PgyApplication.java
@@ -1,16 +1,29 @@
 package com.lsy.app.help.sdk;
 
 import android.app.Application;
+import android.content.Intent;
 
-import com.pgyersdk.crash.PgyCrashManager;
+import com.tencent.tinker.anno.DefaultLifeCycle;
+import com.tencent.tinker.lib.tinker.TinkerInstaller;
+import com.tencent.tinker.loader.app.ApplicationLike;
+import com.tencent.tinker.loader.shareutil.ShareConstants;
 
 /**
  * Created by Administrator on 2016/5/14.
  */
-public class PgyApplication extends Application {
+//这行注解会在编译时生成一个TkApplication
+@DefaultLifeCycle(application = ".TkApplication",
+        flags = ShareConstants.TINKER_ENABLE_ALL,
+        loadVerifyFlag = false)
+public class PgyApplication extends ApplicationLike {
+    public PgyApplication(Application application, int tinkerFlags, boolean tinkerLoadVerifyFlag, long applicationStartElapsedTime, long applicationStartMillisTime, Intent tinkerResultIntent) {
+        super(application, tinkerFlags, tinkerLoadVerifyFlag, applicationStartElapsedTime, applicationStartMillisTime, tinkerResultIntent);
+    }
+
     @Override
     public void onCreate() {
         super.onCreate();
-        PgyCrashManager.register(this);
+        //PgyCrashManager.register(this);
+        TinkerInstaller.install(this);
     }
 }
